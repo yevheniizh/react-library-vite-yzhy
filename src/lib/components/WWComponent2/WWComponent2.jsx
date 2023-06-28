64ec094a8783f81b41831e700a524bb9083cf910
yevhenii-zhyrov <yevheniizhyrov.dev@gmail.com>
2023-06-28 16:28:57 +0300

    Second component

--- src/lib/components/WWComponent2/WWComponent2.jsx
+++ src/lib/components/WWComponent2/WWComponent2.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import classes from './WWComponent.module.css';
+import classes from './WWComponent2.module.css';
 
-export const WWComponent = ({text = 'WWComponent'}) => (
+export const WWComponent2 = ({text = 'WWComponent2'}) => (
     <div className={classes.container}>{text}</div>
 );
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,6 @@
+/* COMPONENTS */
 export { WWComponent } from './components/WWComponent/WWComponent.jsx'
+export { WWComponent2 } from './components/WWComponent2/WWComponent2.jsx'
+
+/* UTILS */
+export { sum } from './utils/calc.util.js'
